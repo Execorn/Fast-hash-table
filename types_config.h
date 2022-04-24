bc6c17a70d4f089789f78b98ea865456a2d6e6cc
Execorn <n.biryukov.04@gmail.com>
2022-04-24 17:22:15 +0300

    node value type changed to 'size_t'

--- types_config.h
+++ types_config.h
@@ -7,14 +7,14 @@
 // ? Also, you can edit function compare_key() function
 // ----------------------------------------------------------------
 
-#define NODE_VALUE_TYPE     char*
+#define NODE_VALUE_TYPE     size_t
 #define NODE_KEY_TYPE       char*
 
-#define POISON_PTR          (void*)         0xDEADDEAD;
-#define POISON_KEY          (NODE_KEY_TYPE) 0xDEADBEEF;
-#define POISON_VALUE        (NODE_KEY_TYPE) 0xDEADBEEF;
+#define POISON_PTR          (void*)           0xDEADDEAD;
+#define POISON_KEY          (NODE_KEY_TYPE)   0xDEADBEEF;
+#define POISON_VALUE        (NODE_VALUE_TYPE) 0xDEADDEAD;
 
-#define NODE_DEFAULT_VALUE  NULL
+#define NODE_DEFAULT_VALUE  0
 #define NODE_DEFAULT_KEY    NULL
 
 static int compare_key(const NODE_KEY_TYPE a, const NODE_KEY_TYPE b) {
