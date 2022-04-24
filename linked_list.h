20cab70d8b8ca6dca427d0ff0e8aec79f44d184b
Execorn <n.biryukov.04@gmail.com>
2022-04-24 17:41:23 +0300

    custom print_node() support added

--- linked_list.h
+++ linked_list.h
@@ -7,15 +7,6 @@
 
 #include "types_config.h"
 
-struct node_t {
-    struct node_t*   next;
-
-    NODE_KEY_TYPE    key;
-    NODE_VALUE_TYPE value;
-};
-
-typedef struct node_t node_t;
-
 node_t* free_list(node_t* head);
 
 node_t* insert_node(node_t* head, const NODE_KEY_TYPE key, const NODE_VALUE_TYPE value);
--- types_config.h
+++ types_config.h
@@ -17,9 +17,35 @@
 #define NODE_DEFAULT_VALUE  0
 #define NODE_DEFAULT_KEY    NULL
 
+
+
+// ! ----------------------------------------------------------------
+//                  ! DO NOT EDIT THIS PART !
+struct node_t {
+    struct node_t*   next;
+
+    NODE_KEY_TYPE     key;
+    NODE_VALUE_TYPE value;
+};
+
+typedef struct node_t node_t;
+// ! ----------------------------------------------------------------
+
+
+
+
+
 static int compare_key(const NODE_KEY_TYPE a, const NODE_KEY_TYPE b) {
     if (a == NULL || b == NULL) {
         return 0;
     }    
     return strcmp(a, b);
+}
+
+static void print_node(const node_t* node, FILE* stream) {
+    if (node == NULL) {
+        return;
+    }
+
+    fprintf(stream, "word: %22s  ->  usages: [%4zu]\n", node->key, node->value);
 }
