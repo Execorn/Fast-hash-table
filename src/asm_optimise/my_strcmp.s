48d6330a8090e788f1c2145ad3882e5c811f5219
Execorn <n.biryukov.04@gmail.com>
2022-05-05 03:18:43 +0300

    NOT STABLE, DO NOT USE

--- src/asm_optimise/my_strcmp.s
+++ src/asm_optimise/my_strcmp.s
@@ -6,22 +6,22 @@ section .text
 ; todo: save and restore R12, which is preserver register
 my_strcmp: ; ? RAX my_strcmp(RDI, RSI)
                 xor R12, R12  
-loop:
+cmp_loop:
                 ; * 'B in the end of register name means ONE BYTE (8 bits) 
                 mov R13B, [RDI + R12]
                 cmp byte [RDI + R12], 0 ; < checking for end of string (\0)
-                jz exit
+                jz return
 
                 cmp byte [RSI + R12], 0 ; < again \0 of string 2 check 
-                jz exit
+                jz return
 
                 cmp R13B, byte [RSI + R12]
-                jnz exit ; * jnz works if R13B != byte[RSI + R12]
+                jnz return ; * jnz works if R13B != byte[RSI + R12]
 
                 inc R12  ; ? move to the next symbol 
-                jmp loop ; repeat loop
+                jmp cmp_loop ; repeat loop
 
-exit:
-                SUB R13B, [RSI + R12]
+return:
+                SUB R13B, [RSI + R12] ; ? get the answer, for example: 'a' - 's'
                 MOVSX RAX, R13B ; ! Using MOVSX allows to keep answer signed, which is important for 'strcmp'
                 ret 
