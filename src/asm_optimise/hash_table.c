da905f5a2d872f07f6bdc2fa170d210010a3a7c2
Execorn <n.biryukov.04@gmail.com>
2022-05-05 03:14:53 +0300

    Massive memory leak fixed (ht_expand/ht_free)

--- src/asm_optimise/hash_table.c
+++ src/asm_optimise/hash_table.c
@@ -21,7 +21,7 @@ static int ht_set_key(node_t** nodes, const size_t capacity, const NODE_KEY_TYPE
 
 ht_t* ht_init(size_t capacity) {
     if (capacity <= 0) {
-        fprintf(stderr, "Capacity must be positive (>= 1).\n");
+        fprintf(stderr, "Capacity must be positive.\n");
         return NULL;
     }
     
@@ -34,7 +34,7 @@ ht_t* ht_init(size_t capacity) {
     new_ht->capacity = capacity;
     new_ht->size     =        0;
 
-    new_ht->nodes = (node_t**) calloc(capacity, sizeof(node_t*));
+    new_ht->nodes = (node_t**) calloc(new_ht->capacity, sizeof(node_t*));
     if (new_ht->nodes == NULL) {
         fprintf(stderr, "Can't allocate memory for linked lists.\n");
         free(new_ht); // ! IMPORTANT: do not forget to free(new_ht), else we'll get memory leak
@@ -50,12 +50,12 @@ ht_t* ht_free(ht_t* table) {
         return NULL;
     }
 
+    for (size_t node_idx = 0; node_idx < table->capacity; ++node_idx) {
+        table->nodes[node_idx] = free_list(table->nodes[node_idx]);
+    }
+
     table->size      = 0;
     table->capacity  = 0;
-
-    for (size_t node = 0; node < table->capacity; ++node) {
-        table->nodes[node] = free_list(table->nodes[node]);
-    }
 
     free(table->nodes);
     free(table);
@@ -68,12 +68,12 @@ int ht_insert_key(ht_t* table, const NODE_KEY_TYPE key, const NODE_VALUE_TYPE va
         fprintf(stderr, "Can't insert key into non-existent table.\n");
         return 1;
     }
-
+    
     if (key == NULL) {
         fprintf(stderr, "Can't insert NODE_DEFAULT_KEY to the table.\n");
         return 1;
     }
-
+    
 #if (HT_DO_EXPAND == 1)
     if (table->size >= table->capacity * LOAD_FACTOR) { // ? If table is filled more than 72% already, we expand it
         if (ht_expand(table)) {
@@ -82,12 +82,12 @@ int ht_insert_key(ht_t* table, const NODE_KEY_TYPE key, const NODE_VALUE_TYPE va
         }
     }
 #endif    
-
+    
     if (ht_set_key(table->nodes, table->capacity, key, value, &table->size)) {
         fprintf(stderr, "Insertion failed.\n");
         return 1;
     };
-
+    
     return 0;
 }
 
@@ -123,9 +123,9 @@ static int ht_expand(ht_t* table) {
     }
     
     for (size_t list_idx = 0; list_idx < table->capacity; ++list_idx) {
-        if (table->nodes[list_idx]) {
+        if (table->nodes[list_idx] != NULL) {
             uint64_t hash_value = GET_HASH(table->nodes[list_idx]->key);
-            nodes[hash_value % new_capacity] = table->nodes[list_idx];
+            nodes[hash_value % new_capacity] = merge_lists(nodes[hash_value % new_capacity], table->nodes[list_idx]);
         }
     }
 
@@ -145,9 +145,14 @@ static int ht_set_key(node_t** nodes, const size_t capacity, const NODE_KEY_TYPE
 
     uint64_t hash_value = GET_HASH(key);
 
-    nodes[hash_value % capacity] = insert_node(nodes[hash_value % capacity], key, value);
+    node_t* current_node = find_node(nodes[hash_value % capacity], key);
+    if (current_node != NULL) {
+        current_node->value = value;
+        return 0;
+    }
 
-    ++(*size); // ! INCREASE HASH TABLE SIZE
+    nodes[hash_value % capacity] = insert_node(nodes[hash_value % capacity], key, value);
+    ++(*size);
 
     return 0;
 }
@@ -165,24 +170,24 @@ NODE_VALUE_TYPE ht_get(ht_t* table, const NODE_KEY_TYPE key) {
     if (table == NULL) {
         fprintf(stderr, "Can't get value from non-existent table.\n");
         return NODE_DEFAULT_VALUE;
-    }
-
+    }   
+    
     if (key == NULL) {
         fprintf(stderr, "Can't get value from NODE_DEFAULT_KEY.\n");
         return NODE_DEFAULT_VALUE;
     }
-
+    
     if (table->nodes == NULL) {
         return NODE_DEFAULT_VALUE;
     }
-
+    
     uint64_t hash_value = GET_HASH(key);
-
+    
     node_t* needed_node = find_node(table->nodes[hash_value % table->capacity], key);
     if (needed_node == NULL) {
         return NODE_DEFAULT_VALUE;
     }
-
+    
     return needed_node->value;
 }
 
